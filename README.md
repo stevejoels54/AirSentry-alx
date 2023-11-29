3bf9a366d203ba6b78796379073b6bad80d2181b
joelsteven <joelofelectronics@gmail.com>
2023-11-29 17:00:51 +0300

    Initial server setup with core functionality

---
+++ README.md
@@ -0,0 +1,78 @@
+# AirSentry - Indoor Air Quality Monitoring Project
+
+## Project Overview
+
+This project features an Indoor Air Quality Monitoring system, including an embedded device, a RESTful API backend using Flask, and a React.js web app. The backend is responsible for handling data from devices, such as readings and notifications, and storing it in a MongoDB Atlas database.
+
+## Getting Started
+
+### Prerequisites
+
+- Python 3.x
+- MongoDB Atlas Account
+- Postman (optional, for testing API endpoints)
+
+### Setup
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://{YOUR_PERSONAL_TOKEN}@github.com/{YOUR_USERNAME}/AirSentry-alx-server.git AirSentry-server
+   cd AirSentr-server
+   ```
+
+2. Create a virtual environment and activate it:
+
+   ```bash
+   python3 -m venv .venv
+   source .venv/bin/activate  # On Windows, use .venv\Scripts\activate
+   ```
+
+3. Install dependencies:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+4. Create a `.ini` file in the root directory with the following content (same content in sample_ini):
+
+   ```ini
+   [PROD]
+   DB_URI = mongodb+srv://<username>:<password>@<your-atlas-cluster-address>/database
+   ```
+
+   Replace `<username>`, `<password>`, and `<your-atlas-cluster-address>` with your MongoDB Atlas credentials.
+
+### Run the Flask App
+
+Run the Flask app:
+
+```bash
+python app.py
+```
+
+The app will be accessible at http://127.0.0.1:5000/airsentry/api/v1/.
+
+API Endpoints
+GET /airsentry/api/v1/: Returns information about the Indoor Air Quality Monitoring API and project.
+GET /airsentry/api/v1/devices: Returns a list of devices.
+POST /airsentry/api/v1/devices: Creates a new device.
+GET /airsentry/api/v1/devices/<device_id>: Returns information about a specific device.
+PUT /airsentry/api/v1/devices/<device_id>: Updates information about a specific device.
+DELETE /airsentry/api/v1/devices/<device_id>: Deletes a specific device.
+GET /airsentry/api/v1/readings: Returns a list of readings.
+POST /airsentry/api/v1/readings: Creates a new reading.
+GET /airsentry/api/v1/notifications: Returns a list of notifications.
+POST /airsentry/api/v1/notifications: Creates a new notification.
+
+##Contributing
+If you'd like to contribute to this project, please follow these guidelines:
+
+Fork the repository.
+Create a new branch for your feature: git checkout -b feature-name.
+Commit your changes: git commit -m 'Add a new feature'.
+Push to the branch: git push origin feature-name.
+Submit a pull request.
+
+##License
+This project is licensed under the MIT License - see the LICENSE file for details.
---
+++ app.py
@@ -0,0 +1,92 @@
+from flask import Flask, jsonify, request, render_template
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+from datetime import datetime
+import os
+import configparser
+
+app = Flask(__name__)
+
+config = configparser.ConfigParser()
+config.read(os.path.abspath(os.path.join(".ini")))
+
+# Replace the following connection string with your MongoDB Atlas connection string imported from the .ini file
+mongo_uri = config['PROD']['DB_URI']
+client = MongoClient(mongo_uri)
+
+# 'test_db' name of your database
+db = client.test_db
+devices_collection = db.devices
+readings_collection = db.readings
+notifications_collection = db.notifications
+
+# Define the base route for all API endpoints
+api_base_url = '/airsentry/api/v1'
+
+@app.route('/')
+def api_info():
+    # Render an HTML template describing the Indoor Air Quality Monitoring API and project
+    return render_template('api_info.html')
+
+# Device CRUD operations
+@app.route(f'{api_base_url}/devices', methods=['GET', 'POST'])
+def manage_devices():
+    if request.method == 'GET':
+        devices = list(devices_collection.find())
+        return dumps(devices)
+    elif request.method == 'POST':
+        device_data = request.json
+        device_id = devices_collection.insert_one(device_data).inserted_id
+        return jsonify({"device_id": str(device_id)})
+
+@app.route(f'{api_base_url}/devices/<device_id>', methods=['GET', 'PUT', 'DELETE'])
+def manage_device(device_id):
+    if request.method == 'GET':
+        device = devices_collection.find_one({"_id": ObjectId(device_id)})
+        return dumps(device)
+    elif request.method == 'PUT':
+        updated_data = request.json
+        devices_collection.update_one({"_id": ObjectId(device_id)}, {"$set": updated_data})
+        return jsonify({"message": "Device updated successfully"})
+    elif request.method == 'DELETE':
+        devices_collection.delete_one({"_id": ObjectId(device_id)})
+        return jsonify({"message": "Device deleted successfully"})
+
+# Reading CRUD operations
+@app.route(f'{api_base_url}/readings', methods=['GET', 'POST'])
+def manage_readings():
+    if request.method == 'GET':
+        readings = list(readings_collection.find())
+        return dumps(readings)
+    elif request.method == 'POST':
+        reading_data = request.json
+
+        if 'timestamp' not in reading_data:
+            reading_data['timestamp'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        else:
+            reading_data['timestamp'] = datetime.strptime(reading_data['timestamp'], "%Y-%m-%d %H:%M:%S")
+
+        reading_id = readings_collection.insert_one(reading_data).inserted_id
+        return jsonify({"reading_id": str(reading_id)})
+    
+# function to get the most recent reading for a device
+@app.route(f'{api_base_url}/readings/<device_id>', methods=['GET'])
+def get_latest_reading(device_id):
+    if request.method == 'GET':
+        reading = readings_collection.find_one({"device_id": device_id}, sort=[("timestamp", -1)])
+        return dumps(reading)
+
+# Notification CRUD operations
+@app.route(f'{api_base_url}/notifications', methods=['GET', 'POST'])
+def manage_notifications():
+    if request.method == 'GET':
+        notifications = list(notifications_collection.find())
+        return dumps(notifications)
+    elif request.method == 'POST':
+        notification_data = request.json
+        notification_id = notifications_collection.insert_one(notification_data).inserted_id
+        return jsonify({"notification_id": str(notification_id)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
